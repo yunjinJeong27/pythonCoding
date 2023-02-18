d4f8f4871cc8bd7fcb676ef84610720649e8bcf5
yunjinJeong27 <125287003+yunjinJeong27@users.noreply.github.com>
2023-02-18 23:13:08 +0900

    chap01.001Python
    변수 선언 및 type 확인

---
+++ chap01.001Python.py
@@ -0,0 +1,13 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a=1
+print(a)
+1
+type(a)
+<class 'int'>
+a='1'
+type(a)
+<class 'str'>
+
+2+2
+4
